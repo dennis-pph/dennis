33397bcc66d1efccb27af6aac5fb4e1783e8b450
ppenghui <18893183061@163.com>
2021-08-25 15:52:32 +0800

    打包

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import index from '@/page/index'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,20 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    // {
+    //   path: '/',
+    //   name: 'index',
+    //   component: index,
+    //   redirect: '/wel/index',
+
+    //   children: [
+    //     {
+    //       path: '/HelloWorld',
+    //       name: 'HelloWorld',
+    //       component: HelloWorld
+    //     }
+    //   ]
+    // },
   ]
 })
