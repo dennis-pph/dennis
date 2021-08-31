99ad52172a980f0740c9c0ba9913cb73d241bbe4
ppenghui <18893183061@163.com>
2021-08-31 14:31:46 +0800

    新增图标

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default {
 
 <style>
 #app {
-  background: red;
+  /* background: red; */
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
